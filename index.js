9123c2225c3aeceb1c57b456f8ab93fc1f77657b
nabily4e-dev <nbl.dev21@gmail.com>
2023-01-01 17:04:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+// findMatching() function
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+  // fuzzyMatch() function
+  function fuzzyMatch(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(name.toLowerCase()));
+  }
+  
+  // matchName() function
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+  }
+  
