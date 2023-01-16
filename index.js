a4fc99d1ce081353b36386204ae2938df027466c
NabiL Ashbat <nbl.dev21@gmail.com>
2023-01-16 14:18:02 +0300

    Update index.js (assignment)

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 // Code your solution here
+
 // findMatching() function
-function findMatching(drivers, name) {
-    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
-  }
+function findMatching(source, sought) {
+  return source.filter(
+    (possibleMatch) => possibleMatch.toLowerCase() === sought.toLowerCase()
+  );
+}
   
   // fuzzyMatch() function
-  function fuzzyMatch(drivers, name) {
-    return drivers.filter(driver => driver.toLowerCase().startsWith(name.toLowerCase()));
-  }
+ function fuzzyMatch(source, testString) {
+  return source.filter(
+    (possibleMatch) =>
+      possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+  );
+}
   
   // matchName() function
-  function matchName(drivers, name) {
-    return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
-  }
-  
+function matchName(source, soughtName) {
+  return source.filter((record) => record.name === soughtName);
+}
