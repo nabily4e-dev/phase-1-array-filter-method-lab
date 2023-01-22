28ab2a204d7c96c4f7a37f9caa080b9c8ee03333
NabiL Ashbat <nbl.dev21@gmail.com>
2023-01-22 23:29:55 +0300

    Update solution

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
 // بسم الله
 
-// findMatching() function
-function findMatching(source, sought) {
-  return source.filter(
-    (possibleMatch) => possibleMatch.toLowerCase() === sought.toLowerCase()
-  );
+function findMatching(drivers, name) {
+	return drivers.filter(driver => driver.toLowerCase().includes(name.toLowerCase()))
 }
+
+function fuzzyMatch(drivers, name) {
+	return drivers.filter(driver => driver.toLowerCase().startsWith(name.toLowerCase()));
+}
+
+function matchName(drivers, name) {
+	return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+}
+
+
+
+// // findMatching() function
+// function findMatching(source, sought) {
+//   return source.filter(
+//     (possibleMatch) => possibleMatch.toLowerCase() === sought.toLowerCase()
+//   );
+// }
   
-  // fuzzyMatch() function
- function fuzzyMatch(source, testString) {
-  return source.filter(
-    (possibleMatch) =>
-      possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
-  );
-}
+//   // fuzzyMatch() function
+//  function fuzzyMatch(source, testString) {
+//   return source.filter(
+//     (possibleMatch) =>
+//       possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+//   );
+// }
   
-  // matchName() function
-function matchName(source, soughtName) {
-  return source.filter((record) => record.name === soughtName);
-}
+//   // matchName() function
+// function matchName(source, soughtName) {
+//   return source.filter((record) => record.name === soughtName);
+// }
